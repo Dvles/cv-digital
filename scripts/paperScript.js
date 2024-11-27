a279a352c721d5d5b569c5b04d93e3a015038e16
Dvles <132080646+Dvles@users.noreply.github.com>
2024-11-27 15:25:59 +0100

    landing page setup paperjs implementation menu links css style similar to main menu

---
+++ scripts/paperScript.js
@@ -0,0 +1,75 @@
+// script.js
+paper.setup('myCanvas');
+
+// Create a raster item:
+var raster = new paper.Raster('./assets/pixelcut.png');
+
+var loaded = false;
+
+raster.on('load', function() {
+    console.log('Image loaded');
+    loaded = true;
+    onResize();
+});
+
+raster.visible = false;
+var lastPos = paper.view.center;
+
+function moveHandler(event) {
+    if (!loaded) return;
+    if (lastPos.getDistance(event.point) < 10) return;
+    lastPos = event.point;
+
+    var size = this.bounds.size.clone();
+    var isLandscape = size.width > size.height;
+
+    size.width = Math.ceil(size.width / (isLandscape ? 2 : 1));
+    size.height = Math.ceil(size.height / (isLandscape ? 1 : 2));
+
+    var path1 = new paper.Path.Rectangle({
+        point: this.bounds.topLeft,
+        size: size,
+        onMouseMove: moveHandler
+    });
+    path1.fillColor = raster.getAverageColor(path1);
+
+    var path2 = new paper.Path.Rectangle({
+        point: isLandscape
+            ? this.bounds.topCenter
+            : this.bounds.leftCenter,
+        size: {
+            width: Math.floor(size.width),
+            height: Math.floor(size.height)
+        },
+        onMouseMove: moveHandler
+    });
+    path2.fillColor = raster.getAverageColor(path2);
+
+    this.remove();
+}
+
+function onResize(event) {
+    if (!loaded) return;
+    console.log('Resizing view');
+    paper.project.activeLayer.removeChildren();
+
+    paper.view.size = new paper.Size(window.innerWidth, window.innerHeight);
+
+    raster.fitBounds(paper.view.bounds, true);
+
+    new paper.Path.Rectangle({
+        rectangle: paper.view.bounds,
+        fillColor: raster.getAverageColor(paper.view.bounds),
+        onMouseMove: moveHandler
+    });
+}
+
+window.addEventListener('resize', onResize);
+onResize();
+
+function scrollToContent() {
+    document.getElementById('mainContent').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
