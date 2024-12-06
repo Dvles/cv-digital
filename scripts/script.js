695f77a03f3b1aeb0a36a296cda0fdebe2606fb9
Dvles <132080646+Dvles@users.noreply.github.com>
2024-12-06 17:06:06 +0100

    modal creation with summary with js nb: review content

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,37 @@ console.log("Hello IT world");
 console.log("Let's work together!");
 console.info("AVAILABLE: FROM XX to XX");
 
+
+
+// ➔➔ MODAL
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
+
+
 // ➔➔ MENU SCROLL HIGHLIGHT
 
 $(document).ready(function () {
